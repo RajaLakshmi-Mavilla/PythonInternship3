71b733c9352fb4365d812e265e0e424de54a7679
RajaLakshmi-Mavilla <154977219+RajaLakshmi-Mavilla@users.noreply.github.com>
2024-02-01 16:42:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+##Random Password Generator
+import string
+import random
+
+characters = string.ascii_letters + string.digits + string.punctuation
+
+# Enter password in the range of 6 to 15
+length = int(input("Enter the length of the password: "))
+
+if 6 <= length <= 15:
+    password = ''.join(random.choices(characters, k=length))
+    print("Your password is:", password)
+else:
+    print("Enter a valid length between 6 and 15.")
